8f2d893f269919f329e38fae4682fe727cd8f381
Aadvika <deepikamallick2001@gmail.com>
2025-08-16 22:06:35 +0530

    Update simpleDoWhileLoop.java

--- Day4-Loops/simpleDoWhileLoop.java
+++ Day4-Loops/simpleDoWhileLoop.java
@@ -1,5 +1,12 @@
 package loops;
 
 public class simpleDoWhileLoop {
+        public static void main(String[] args) {
+            int c = 8;
 
+            do {
+                System.out.println("Hello Aadvika");
+            }
+            while (c >= 3);
+        }
 }
